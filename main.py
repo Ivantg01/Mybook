ebedca447f6293c3db490596c25c9fce01389bbb
Ivantg01 <96993149+Ivantg01@users.noreply.github.com>
2023-04-26 09:33:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-#Based on https://github.com/techwithtim/Flask-Web-App-Tutorial
-#video https://www.youtube.com/watch?v=dam0GPOAvVI
-#Based on https://github.com/alexroel/blog-flask
-#video https://www.youtube.com/watch?app=desktop&v=JTAY5_LO0Ug
+#### Imports
 from flask import send_from_directory
 from website import create_app
+from config import config
 
-app = create_app()
+#### Variables
+app = create_app()   #create the flask app
 
-#Definimos la ruta donde visualizar las caratulas
-@app.route('/.bookfiles/<path:filename>')
+#### Route to display book covers
+@app.route('/cover/<path:filename>')
 def download_file(filename):
-    return send_from_directory(app.root_path+"/../bookfiles/", filename, as_attachment=True)
+    return send_from_directory(config.BOOK_PATH, filename, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#Start the flask app
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5001)
+
+
+
 
