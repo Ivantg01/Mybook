ad5e49a127f0d9303d8419e7a7209d7134ee46c8
Ivantg01 <96993149+Ivantg01@users.noreply.github.com>
2023-05-02 20:22:32 +0200

    Add files via upload
    Creamos gconfig y reemplazamos por config para evitar confundirlo con decouple.config

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #### Imports
 from flask import send_from_directory
 from website import create_app
-from config import config
+from gconfig import gconfig
 
 #### Variables
 app = create_app()   #create the flask app
@@ -9,7 +9,7 @@ app = create_app()   #create the flask app
 #### Route to display book covers
 @app.route('/cover/<path:filename>')
 def download_file(filename):
-    return send_from_directory(config.BOOK_PATH, filename, as_attachment=True)
+    return send_from_directory(gconfig.BOOK_PATH, filename, as_attachment=True)
 
 #Start the flask app
 if __name__ == "__main__":
